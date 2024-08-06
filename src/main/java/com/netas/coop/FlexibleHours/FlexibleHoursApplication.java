7bdc391638bc2ea5458a8a1e584bce8497e1b3df
Mustafa KAÇAR <mustafakacar48@outlook.com.tr>
2024-08-06 12:01:55 +0300

    Added .gitignore, .mvn/wrapper/maven-wrapper.properties, docker-compose.yaml, mvnw.

---
+++ src/main/java/com/netas/coop/FlexibleHours/FlexibleHoursApplication.java
@@ -0,0 +1,13 @@
+package com.netas.coop.FlexibleHours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexibleHoursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexibleHoursApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/netas/coop/FlexibleHours/FlexibleHoursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netas.coop.FlexibleHours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexibleHoursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
